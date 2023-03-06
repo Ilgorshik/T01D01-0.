c532b34409477d83cfa170c8e7fa048da94da550
Runcel Kaye <runcelka@ev-k6.kzn.21-school.ru>
2023-03-06 17:54:31 +0300

    Quest1-6 and little bit of 7

---
+++ src/quest3.sh
@@ -0,0 +1,7 @@
+mv door_management_fi door_management_files~
+runcelka@ev-k6 door_management_files % mkdir door_configuration
+runcelka@ev-k6 door_management_files % mkdir door_map
+runcelka@ev-k6 door_management_files % mkdir door_logs
+runcelka@ev-k6 door_management_files % mv *.log door_logs 
+runcelka@ev-k6 door_management_files % mv *.conf door_configuration
+runcelka@ev-k6 door_management_files % mv door_map_1.1 door_map
---
+++ src/quest6.sh
@@ -0,0 +1,15 @@
+ cd ai_help
+ ls
+ chmod 777 keygen.sh
+ chmod 777 unifier.sh
+ cat keygen.sh
+ vim keygen.sh
+ cat unifier.sh
+ ./keygen.sh
+ cd key
+ man rm
+ rm file*
+ ls
+ cd ..
+ ls
+ ./unifier.sh
